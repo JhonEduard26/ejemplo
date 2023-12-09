9b7752680a3e6888fb8e5bdad7ea6c0f29fc1ab0
jhon <jhon.bocanegra26@gmail.com>
2023-12-09 16:49:59 -0500

    animacion agregada

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-const item = document.querySelector(".card-subtitle");
-item.addEventListener("click", (e) => {});
+const cardBody = document.querySelector(".card-body");
+const cardParragraph = document.querySelector(".card-parragraph");
+
+cardBody.addEventListener("click", () => {
+  cardBody.children[0].children[0].classList.toggle("hidden")
+  cardBody.children[0].children[1].classList.toggle("hidden")
+  cardParragraph.classList.toggle("hidden")
+});
